523fcec6915f89e37305d5c290e371a83f82568d
Bogdan <bogdan1850@gmail.com>
2022-06-01 10:42:54 +0200

    fix show static content

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ let persons = [
 
 
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)  })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
